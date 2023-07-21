45df1a3ef76bdaddaa25af51ced79008bac7ab7a
Parisa <mousaviparisa5@yahoo.com>
2023-07-21 19:30:55 +0330

    Implement ConnectToServer.java and add FormEvent & Message

---
+++ src/main/java/com/parim/ConnectToClient.java
@@ -0,0 +1,42 @@
+package com.parim;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parim.event.Message;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ConnectToClient {
+    private Socket socket;
+    private ObjectMapper mapper = new ObjectMapper();
+    private PrintWriter output;
+    private Scanner input;
+
+    public ConnectToClient(Socket socket){
+        this.socket = socket;
+        try {
+            output = new PrintWriter(socket.getOutputStream(), true);
+            input = new Scanner(socket.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void send(Message message){
+        try {
+            output.println(mapper.writeValueAsString(message));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public Message receive(){
+        try {
+            return mapper.readValue(input.nextLine(), Message.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
